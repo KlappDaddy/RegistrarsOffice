e27aea154d6578760ccd61962a7374819ffe0756
KlappDaddy <ryan.klapper.MA@gmail.com>
2018-03-30 13:45:59 -0700

    Add files via upload

---
+++ Globals.h
@@ -0,0 +1,9 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#include <fstream>
+#include <iostream>
+using namespace std;
+extern ifstream InputFile;
+extern int NumOfWindows;
+extern string Windows;
+#endif // GLOBALS_H
